231b9a9bd17b439d28cfe4f05a27a0e1a4509957
Marc Bodmer <marc.bodmer@gmail.com>
2012-10-29 14:07:24 -0400

    Cleaned up default files

--- config/routes.rb
+++ config/routes.rb
@@ -48,9 +48,8 @@ Flowshow::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # The root of your site routed with "root"
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
